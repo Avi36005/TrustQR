048dff9aa466bfa92aad3db7bc67ffabeef9a27c
Avi36005 <2023.avinash.gehi@ves.ac.in>
2026-05-30 12:25:03 +0530

    added choose screen route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ReportCard from './pages/ReportCard'
 import Details from './pages/Details'
 import History from './pages/History'
 import About from './pages/About'
+import Choose from './pages/Choose'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <div className="app-container">
               <Routes>
                 <Route path="/" element={<Scanner />} />
+                <Route path="choose" element={<Choose />} />
                 <Route path="report" element={<ReportCard />} />
                 <Route path="details" element={<Details />} />
                 <Route path="history" element={<History />} />

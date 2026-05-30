3973e7e66a2531866f4c2a054edde5df8b6e3055
Avi36005 <2023.avinash.gehi@ves.ac.in>
2026-05-30 12:58:57 +0530

    grey background fix on progress bar

--- Frontend/src/components/ProgressBar.jsx
+++ Frontend/src/components/ProgressBar.jsx
@@ -1,7 +1,7 @@
 export default function ProgressBar({ visible }) {
   if (!visible) return null
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 h-0.5 bg-gray-100 overflow-hidden">
+    <div className="fixed top-0 left-0 right-0 z-50 h-0.5 bg-[#EEEEEE] overflow-hidden">
       <div
         className="h-full bg-black progress-indeterminate"
         style={{ width: '30%' }}
